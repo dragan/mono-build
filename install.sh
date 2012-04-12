e91898a4959ffa40a82977596b37c5ecdd056000
Dale Ragan <dale@ragan.io>
2012-04-12 17:24:37 -0400

    Manual page for additional instructions.

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ fi
 
 BIN_PATH="${PREFIX}/bin"
 SHARE_PATH="${PREFIX}/share/mono-build"
+SHARE_MAN_PATH="${PREFIX}/share/man/man1"
 
 mkdir -p "${BIN_PATH}"
 mkdir -p "${SHARE_PATH}"
+mkdir -p "${SHARE_MAN_PATH}"
 
 for file in bin/*; do
 	cp "${file}" "${BIN_PATH}"
@@ -19,5 +21,7 @@ done
 for file in share/mono-build/*; do
 	cp "${file}" "${SHARE_PATH}"
 done
+
+cp share/man/mono-build.1 "${SHARE_MAN_PATH}"
 
 echo "Installed mono-build at ${PREFIX}"
--- uninstall.sh
+++ uninstall.sh
@@ -7,11 +7,14 @@ if [ -z "${PREFIX}" ]; then
 fi
 
 SHARE_PATH="${PREFIX}/share/mono-build"
+SHARE_MAN_PATH="${PREFIX}/share/man/man1"
 
 for file in bin/*; do
 	rm "${PREFIX}"/"${file}"
 done
 
 rm -rf "${SHARE_PATH}"
+
+rm "${SHARE_MAN_PATH}/mono-build.1"
 
 echo "Uninstalled mono-build at ${PREFIX}"
